6fce615332b36e9ab746a6d0850ba1a6b24223be
tarek <tarekt820@gmail.com>
2018-12-09 00:00:34 +0200

    change the whole style for form and when generate clicked and need to edit the resposnive whole thing

--- main/js/main.js
+++ main/js/main.js
@@ -10,23 +10,33 @@ let book = {
 
 // the new Classes added after submit button 
 const newClasses = function () {
-    // change the style of the form 
-    document.getElementById('book-info').classList.add('book-info-shown')
+    
 
+    // add class to the div contain the form
     document.querySelector('.show-div').classList.add('after-sub') 
 
-    var form_item = document.getElementsByClassName('form-item')
+    // add class to the form
+    document.getElementById('book-info').classList.add('book-info-shown')
 
+    // add class to the left part of the form 
     document.querySelector('.part-right').classList.add('part-right-shown')
 
+    // add class to the right part of the form 
     document.querySelector('.part-left').classList.add('part-left-shown')
+    
+    // add class to the button of generate 
+    document.querySelector('.btn-sub').classList.add('btn-sub-shown')
+
+    // add class to the form item 
+    document.querySelector('.form-item').classList.add('form-item-shown')
+
+
 
     
 
 
 }
 
-let x = document.getElementById('book-info')
 
 //add event to button when clicked 
 document.querySelector('.btn-sub').addEventListener('click' , function(e) {
