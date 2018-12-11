c33dfc854971afd591286a7d5eb34dc64b2b75a9
tarek <tarekt820@gmail.com>
2018-12-11 21:31:11 +0200

    final design

--- main/js/main.js
+++ main/js/main.js
@@ -8,40 +8,104 @@ let book = {
 
 }
 
+
 // the new Classes added after submit button 
 const newClasses = function () {
     
 
     // add class to the div contain the form
-    document.querySelector('.show-div').classList.add('after-sub') 
+    const elParent = document.querySelector('.show-div')
+    elParent.classList.add('after-sub') 
 
-    // add class to the form
-    document.getElementById('book-info').classList.add('book-info-shown')
+    // add class to the form 
+    const elForm = document.getElementById('book-info')
+    elForm.classList.add('book-info-shown')
 
     // add class to the left part of the form 
-    document.querySelector('.part-right').classList.add('part-right-shown')
+    const elRight = document.querySelector('.part-right')
+    elRight.classList.add('part-right-shown')
 
     // add class to the right part of the form 
-    document.querySelector('.part-left').classList.add('part-left-shown')
+    const elLeft = document.querySelector('.part-left')
+    elLeft.classList.add('part-left-shown')
     
     // add class to the button of generate 
-    document.querySelector('.btn-sub').classList.add('btn-sub-shown')
+    const elButton = document.querySelector('.btn-sub')
+    elButton.classList.add('btn-sub-shown')
 
     // add class to the form item 
-    document.querySelector('.form-item').classList.add('form-item-shown')
+    const elItem = document.querySelector('.form-item')
+    elItem.classList.add('form-item-shown')
+
+    // add class to the results 
+    const elRes = document.getElementById('results')
+    elRes.classList.add('results-shown')
+
+}
+
+// Save the inputs value to our book object 
+const saveInfo = ()=> {
+ // save the title 
+ const elTitle = document.getElementById('title')
+ book.title = elTitle.value
+ const elSubtitle = document.getElementById('subtitle')
+ book.subtitle = elSubtitle.value
+ const elBackground = document.getElementById('background')
+ book.background_color = elBackground.value
+ const elPublish = document.getElementById('publisher')
+ book.publisher = elPublish.value
+ const elYear = document.getElementById('publishing-year')
+ book.publishing_year = elYear.value
+ const elAuthor = document.getElementById('author')
+ book.author = elAuthor.value
+}
 
 
 
-    
 
 
+// results.section 
+const elDomView = function (book) {
+    const elDiv = document.createElement('div')
+    elDiv.classList.add('Parent')
+    elDiv.innerHTML = `<div class='item'>
+    <h3>Title</h3>
+    <p>${book.title}</p>
+    </div>
+    <div class='item'>
+    <h3>Author</h3>
+    <p>${book.author}</p>
+    </div>
+    <div class='item'>
+    <h3>Subtitle</h3>
+    <p> ${book.subtitle} </p>
+    </div>
+    <div class='item'>
+    <h3>Background</h3>
+    <p>${book.background_color}</p>
+    </div>
+    <div class='item'>
+    <h3>Publisher</h3>
+    <p>${book.publisher}</p>
+    </div>
+    <div class='item'>
+    <h3>Publishing year</h3>
+    <p>${book.publishing_year}</p>
+    </div>`
+
+    document.getElementById('results').appendChild(elDiv)
+   
 }
 
 
 //add event to button when clicked 
 document.querySelector('.btn-sub').addEventListener('click' , function(e) {
-    
+    document.getElementById('main-page').style.width = '50%'
+    document.getElementById('body').style.display = 'flex'
     newClasses()
+    saveInfo()
+    elDomView(book)
+    
 
 
 })
